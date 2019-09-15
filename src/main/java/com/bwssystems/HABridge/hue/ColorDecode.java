7917048a0059c3510f87853704bc791e84e0e46c
Michael Belz <michael@mbelz.de>
2019-09-15 23:49:46 +0200

    - get device ID from url deviceID parameter - do not process url color decoding if deviceID is not supplied

--- src/main/java/com/bwssystems/HABridge/hue/ColorDecode.java
+++ src/main/java/com/bwssystems/HABridge/hue/ColorDecode.java
@@ -1,6 +1,7 @@
 package com.bwssystems.HABridge.hue;
 
 import java.util.List;
+import java.util.regex.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,68 +12,83 @@ public class ColorDecode {
 	
 	private static final Logger log = LoggerFactory.getLogger(ColorDecode.class);
 
-	private static String handleCTRequest(Integer ct, String request) {
+	private static String handleCTRequest(Integer ct, String request, String deviceID) {
 
-		String[] splitRequest = request.split("\\?");
-
-		String id = splitRequest[1];
-
-		String ctParam = String.format("%s.ct=%s", id, ct.toString());
+		String ctParam = String.format("%s.ct=%s", deviceID, ct.toString());
 	
-		return String.format("%s?%s", splitRequest[0], ctParam);
+		return String.format("%s?%s", getHostFromRequest(request), ctParam);
 
 	}
 
 
-	private static String handleHSRequest(HueSatBri hs, String request) {
-
-		String[] splitRequest = request.split("\\?");
-
-		String id = splitRequest[1];
+	private static String handleHSRequest(HueSatBri hs, String request, String deviceID) {
 
 		float hue = (float)hs.getHue() / (float)182.04;
 		float sat = (float)hs.getSat();
 
-		String hueParam = String.format("%s.hue=%f", id, hue);
-		String satParam = String.format("%s.sat=%f", id, sat);
+		String hueParam = String.format("%s.hue=%f", deviceID, hue);
+		String satParam = String.format("%s.sat=%f", deviceID, sat);
 	
-		return String.format("%s?%s&%s", splitRequest[0], hueParam, satParam);
+		return String.format("%s?%s&%s", getHostFromRequest(request), hueParam, satParam);
 
 	}
 
 
-	private static String handleCIERequest(List<Double> cie, String request) {
+	private static String handleCIERequest(List<Double> cie, String request, String deviceID) {
 
+		String xyParam = String.format("%s.xy=%f,%f", deviceID, cie.get(0), cie.get(1));
+	
+		return String.format("%s?%s", getHostFromRequest(request), xyParam);
+
+	}
+
+
+	private static String getDeviceIDFromRequest(String request){
+
+		// request format: "http://localhost:8087/setBulk?${color.deviceID=deconz.0.Lights.1}";
+
+		Pattern pattern = Pattern.compile("\\$\\{color.deviceID=(.*)}");
+		Matcher matcher = pattern.matcher(request);
+
+		String deviceID = "";
+
+		if (matcher.find()){
+			deviceID = matcher.group(1);
+		}		
+
+		return deviceID;
+  
+	}
+
+
+	private static String getHostFromRequest(String request) {
+	
 		String[] splitRequest = request.split("\\?");
 
-		String id = splitRequest[1];
-
-		String xyParam = String.format("%s.xy=%f,%f", id, cie.get(0), cie.get(1));
-	
-		return String.format("%s?%s", splitRequest[0], xyParam);
-
+		return splitRequest[0];	
 	}
 
 
 	@SuppressWarnings("unchecked")
 	public static String replaceColorData(String request, ColorData colorData, int setIntensity, boolean isHex) {
 	
-		if (request == null) {
-			return null;
+		if (request == null || colorData == null) {
+			return request;
 		}
 
-		if (colorData == null) {
+		String deviceID = getDeviceIDFromRequest(request);
+		if (deviceID.isEmpty()) {
 			return request;
 		}
 
 		ColorData.ColorMode colorMode = colorData.getColorMode();
 
 		if (colorMode == ColorData.ColorMode.XY) {
-			return handleCIERequest((List<Double>) colorData.getData(), request);
+			return handleCIERequest((List<Double>) colorData.getData(), request, deviceID);
 		} else if (colorMode == ColorData.ColorMode.CT) {
-			return handleCTRequest((Integer) colorData.getData(), request);
+			return handleCTRequest((Integer) colorData.getData(), request, deviceID);
 		} else if (colorMode == ColorData.ColorMode.HS) {
-			return handleHSRequest((HueSatBri) colorData.getData(), request);
+			return handleHSRequest((HueSatBri) colorData.getData(), request, deviceID);
 		} else {
 			log.debug("ColorMode:" + colorMode + " not handled, request failed!");
 			return "";
